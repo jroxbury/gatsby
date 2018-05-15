dccc10cffc05e61044aa7aea7fe295348fa11550
Joe Roxbury <jroxbury@snapfinance.com>
2018-05-14 22:16:54 -0600

    do you gotta build?

--- src/components/header.js
+++ src/components/header.js
@@ -23,7 +23,7 @@ const Header = ({ siteTitle }) => (
             textDecoration: 'none',
           }}
         >
-          Dang, this is cool!
+          wtf!
         </Link>
       </h1>
     </div>
